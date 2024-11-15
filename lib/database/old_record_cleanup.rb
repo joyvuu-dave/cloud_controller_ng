49ede51b5b4c9aab976c784e83b0f573b6d52d57
David Riddle <david.riddle@broadcom.com>
2024-11-15 15:05:31 -0600

    Keep Usage Event records corresponding to running Apps & Services

--- lib/database/old_record_cleanup.rb
+++ lib/database/old_record_cleanup.rb
@@ -3,12 +3,13 @@ require 'database/batch_delete'
 module Database
   class OldRecordCleanup
     class NoCurrentTimestampError < StandardError; end
-    attr_reader :model, :days_ago, :keep_at_least_one_record
+    attr_reader :model, :days_ago, :keep_at_least_one_record, :keep_running_records
 
-    def initialize(model, days_ago, keep_at_least_one_record: false)
+    def initialize(model, days_ago, keep_at_least_one_record: false, keep_running_records: false)
       @model = model
       @days_ago = days_ago
       @keep_at_least_one_record = keep_at_least_one_record
+      @keep_running_records = keep_running_records
     end
 
     def delete
@@ -19,6 +20,9 @@ module Database
         last_record = model.order(:id).last
         old_records = old_records.where(Sequel.lit('id < ?', last_record.id)) if last_record
       end
+
+      old_records = exclude_running_records(old_records) if keep_running_records
+
       logger.info("Cleaning up #{old_records.count} #{model.table_name} table rows")
 
       Database::BatchDelete.new(old_records, 1000).delete
@@ -34,6 +38,56 @@ module Database
 
     def logger
       @logger ||= Steno.logger('cc.old_record_cleanup')
+    end
+
+    def exclude_running_records(old_records)
+      return old_records unless has_duration?(model)
+
+      beginning_string = beginning_string(model)
+      ending_string = ending_string(model)
+      guid_symbol = guid_symbol(model)
+
+      raise "Invalid duration model: #{model}" if beginning_string.nil? || ending_string.nil? || guid_symbol.nil?
+
+      initial_records = old_records.where(state: beginning_string).from_self(alias: :initial_records)
+      final_records = old_records.where(state: ending_string).from_self(alias: :final_records)
+
+      exists_condition = final_records.where(Sequel[:final_records][guid_symbol] => Sequel[:initial_records][guid_symbol]).where do
+        Sequel[:final_records][:created_at] >= Sequel[:initial_records][:created_at]
+      end.select(1).exists
+
+      prunable_initial_records = initial_records.where(exists_condition)
+      other_records = old_records.exclude(state: [beginning_string, ending_string])
+
+      prunable_initial_records.union(final_records, all: true).union(other_records, all: true)
+    end
+
+    def has_duration?(model)
+      return true if model == VCAP::CloudController::AppUsageEvent
+      return true if model == VCAP::CloudController::ServiceUsageEvent
+
+      false
+    end
+
+    def beginning_string(model)
+      return VCAP::CloudController::ProcessModel::STARTED if model == VCAP::CloudController::AppUsageEvent
+      return VCAP::CloudController::Repositories::ServiceUsageEventRepository::CREATED_EVENT_STATE if model == VCAP::CloudController::ServiceUsageEvent
+
+      nil
+    end
+
+    def ending_string(model)
+      return VCAP::CloudController::ProcessModel::STOPPED if model == VCAP::CloudController::AppUsageEvent
+      return VCAP::CloudController::Repositories::ServiceUsageEventRepository::DELETED_EVENT_STATE if model == VCAP::CloudController::ServiceUsageEvent
+
+      nil
+    end
+
+    def guid_symbol(model)
+      return :app_guid if model == VCAP::CloudController::AppUsageEvent
+      return :service_instance_guid if model == VCAP::CloudController::ServiceUsageEvent
+
+      nil
     end
   end
 end
