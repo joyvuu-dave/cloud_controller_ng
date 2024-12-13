46169c6d92a57aa6048ee632cf2b79294c21d539
David Riddle <david.riddle@broadcom.com>
2024-12-13 15:14:54 -0600

    typo

--- db/migrations/20241205162359_create_app_usage_consumers.rb
+++ db/migrations/20241205162359_create_app_usage_consumers.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     create_table :app_usage_consumers do |_t|
       primary_key :id, name: :id
       String :consumer_id, null: false, size: 255
-      String :last_app_usage_event_id, null: fals, size: 255
+      String :last_app_usage_event_id, null: false, size: 255
 
       DateTime :created_at, null: false
       DateTime :updated_at, null: false
