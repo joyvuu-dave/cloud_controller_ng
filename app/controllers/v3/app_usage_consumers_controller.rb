ac1a4c833aab7cd9a3a81b68756a9bad95fbc5d8
David Riddle <david.riddle@broadcom.com>
2024-12-19 19:36:32 -0600

    get it working

--- app/controllers/v3/app_usage_consumers_controller.rb
+++ app/controllers/v3/app_usage_consumers_controller.rb
@@ -1,16 +1,10 @@
 require 'controllers/base/base_controller'
 
-class AppUsageConsumersController < VCAP::CloudController::RestController::BaseController
+class AppUsageConsumersController < ApplicationController
   def destroy
-    check_authentication(:destroy)
-
     app_usage_consumer = AppUsageConsumer.find(consumer_guid: hashed_params[:guid])
 
     resource_not_found!(:app_usage_consumer) unless app_usage_consumer
-
-    unless roles.admin?
-      raise CloudController::Errors::ApiError.new_from_details('NotAuthorized')
-    end
 
     app_usage_consumer.db.transaction do
       app_usage_consumer.lock!
--- app/controllers/v3/service_usage_consumers_controller.rb
+++ app/controllers/v3/service_usage_consumers_controller.rb
@@ -1,12 +1,10 @@
+require 'controllers/base/base_controller'
+
 class ServiceUsageConsumersController < ApplicationController
   def destroy
     service_usage_consumer = ServiceUsageConsumer.find(consumer_guid: hashed_params[:guid])
 
     resource_not_found!(:service_usage_consumer) unless service_usage_consumer
-
-    unless roles.admin?
-      raise CloudController::Errors::ApiError.new_from_details('NotAuthorized')
-    end
 
     service_usage_consumer.db.transaction do
       service_usage_consumer.lock!
